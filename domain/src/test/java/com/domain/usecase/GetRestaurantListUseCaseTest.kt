2ec4cb1b11f842cdbc24cc72c73f8a5534d37947
liuwenping <liuwenping828@gmail.com>
2019-08-14 23:24:27 +0800

    Android domain 模块中Api 接口测试用例。

---
+++ domain/src/test/java/com/domain/usecase/GetRestaurantListUseCaseTest.kt
@@ -0,0 +1,75 @@
+package com.domain.usecase
+
+import com.domain.repository.restaurant.model.Restaurant
+import com.domain.repository.restaurant.repository.RestaurantRepository
+import com.domain.repository.restaurant.usecase.GetRestaurantListUseCase
+import com.domain.repository.restaurant.usecase.GetRestaurantListUseCase.Result.Success
+import com.domain.repository.restaurant.usecase.GetRestaurantListUseCase.Result.Failure
+import com.domain.rx.RxJavaTestHooksResetRule
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetRestaurantListUseCaseTest {
+
+    @get:Rule
+    var rxJavaTestHooksResetRule = RxJavaTestHooksResetRule()
+
+    @Mock
+    lateinit var restaurantRepository: RestaurantRepository
+
+    private lateinit var sut: GetRestaurantListUseCase
+
+    @Before
+    fun setUp() {
+        sut = GetRestaurantListUseCase(restaurantRepository)
+    }
+
+    @Test
+    fun `retrieves the restaurant list`() {
+        given(restaurantRepository.getRestaurantList(any(), any(), any(), any())).willReturn(Single.error(Throwable()))
+
+        sut.execute(1.1, 1.1, 0, 50).test()
+
+        verify(restaurantRepository).getRestaurantList(any(), any(), any(), any())
+    }
+
+    @Test
+    fun `shows loading to start`() {
+        given(restaurantRepository.getRestaurantList(any(), any(), any(), any()))
+            .willReturn(Single.just(mock()))
+
+        sut.execute(1.1, 1.1, 0, 50).test()
+    }
+
+    @Test
+    fun `returns the success result when success retrieving the restaurant list`() {
+        val restaurantList = arrayListOf<Restaurant>()
+
+        given(restaurantRepository.getRestaurantList(any(), any(), any(), any()))
+            .willReturn(Single.just(restaurantList))
+
+        sut.execute(1.1, 1.1, 0, 50).test()
+            .assertValueAt(1) { (it as Success).restaurants == restaurantList }
+    }
+
+    @Test
+    fun `returns the failure result when error retrieving the restaurant list`() {
+        val throwable = Throwable()
+        given(restaurantRepository.getRestaurantList(any(), any(), any(), any()))
+            .willReturn(Single.error(throwable))
+
+        sut.execute(1.1, 1.1, 0, 50).test()
+            .assertValueAt(1) { (it as Failure).throwable == throwable }
+    }
+
+}
---
+++ domain/src/test/java/com/domain/usecase/GetRestaurantUseCaseTest.kt
@@ -0,0 +1,70 @@
+package com.domain.usecase
+
+import com.domain.repository.restaurant.model.Restaurant
+import com.domain.repository.restaurant.repository.RestaurantRepository
+import com.domain.repository.restaurant.usecase.GetRestaurantUseCase
+import com.domain.repository.restaurant.usecase.GetRestaurantUseCase.Result.Loading
+import com.domain.repository.restaurant.usecase.GetRestaurantUseCase.Result.Success
+import com.domain.repository.restaurant.usecase.GetRestaurantUseCase.Result.Failure
+import com.domain.rx.RxJavaTestHooksResetRule
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetRestaurantUseCaseTest {
+
+    @get:Rule
+    var rxJavaTestHooksResetRule = RxJavaTestHooksResetRule()
+
+    @Mock
+    lateinit var restaurantRepository: RestaurantRepository
+
+    private lateinit var sut: GetRestaurantUseCase
+
+    @Before
+    fun setUp() {
+        sut = GetRestaurantUseCase(restaurantRepository)
+    }
+
+    @Test
+    fun `retrieves the restaurant`() {
+        given(restaurantRepository.getRestaurant(any())).willReturn(Single.error(Throwable()))
+
+        sut.execute(1).test()
+
+        verify(restaurantRepository).getRestaurant(any())
+    }
+
+    @Test
+    fun `shows loading to start`() {
+        given(restaurantRepository.getRestaurant(any())).willReturn(Single.just(mock()))
+
+        sut.execute(1).test().assertValueAt(0) { (it == Loading) }
+    }
+
+    @Test
+    fun `returns the success result when success retrieving the restaurant`() {
+        val restaurant = mock<Restaurant>()
+        given(restaurantRepository.getRestaurant(any())).willReturn(Single.just(restaurant))
+
+        sut.execute(1).test().assertValueAt(1) { (it as Success).restaurants == restaurant }
+    }
+
+    @Test
+    fun `returns the failure result when error retrieving the restaurant`() {
+        val throwable = Throwable()
+        given(restaurantRepository.getRestaurant(any())).willReturn(Single.error(throwable))
+
+        sut.execute(1).test().assertValueAt(1) { (it as Failure).throwable == throwable }
+    }
+
+}
